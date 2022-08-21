67c0cc3c4db40495f154239a2be0af0aad0968ec
Palash Mandal <palash15-2092@diu.edu.bd>
2022-08-22 02:01:46 +0600

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Particle from '../Particle';
-import pdf from '../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf';
+import pdf from '../../Assets/../Assets/Palash_Mandal.pdf';
 import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = 'LINK_HERE';
+const resumeLink =
+  'https://raw.githubusercontent.com/pmdev71/my_portfolio/master/src/Assets/Palash_Mandal.pdf';
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
