2cbc25ee26e0f84aaada79508affb4116bdac06a
Palash Mandal <palash15-2092@diu.edu.bd>
2022-08-22 01:45:45 +0600

    added resume

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { ImPointRight } from 'react-icons/im';
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: 'justify' }}>
+            Hi Everyone, I'm <span className="purple">Palash Mandal </span>
+            from <span className="purple"> Dhaka, Bangladesh.</span>
+            <br />
+            I'm a junior full stack web developer with a passion for building
+            beautiful, responsive websites. Always I'm looking to learn new
+            things and improve my skills.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writting Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: 'rgb(155 126 172)' }}>
+            "Strive to build things that make a difference!"{' '}
+          </p>
+          <footer className="blockquote-footer">Palsah Mandal</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
